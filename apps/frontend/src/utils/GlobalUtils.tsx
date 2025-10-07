670f93168beb8fbc76d107efcd34fc49fc320ae6
Biggie Cheese <Jv3studios@gmail.com>
2025-10-06 21:54:16 -0500

    refactoring

--- apps/frontend/src/utils/GlobalUtils.tsx
+++
@@ -1,9 +0,0 @@
-import { db } from "@/lib/firebase";
-import { DocumentReference, doc } from "firebase/firestore";
-
-/**
- * @returns a DocumentRefernece for the given user ID.
- */
-export const userDocRef = (userId: string): DocumentReference => {
-  return doc(db, "users", userId);
-};
