10d170d614f62b6a9329d872d9163305b921e031
Biggie Cheese <Jv3studios@gmail.com>
2025-11-19 23:57:20 -0600

    Removed dead code from backend folder and migrated server-side code into functions folder

--- backend/src/utils/curve.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @param level - an integer number representing the needed xp to move on
-*/
-export function xpNeededFor(level: number): number {
-  const base = 100;
-  const growth = 25;
-  return base + growth * (level - 1);
-}
--- backend/src/utils/global.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "@/lib/firebaseAdmin"
-
-// This returns the user reference
-export const UserRef = (uid: string) => {
-    return db.collection("users").doc(uid);
-}
